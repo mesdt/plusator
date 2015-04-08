88cd4a3d57c081fa5fd7d24c6d7a41313979c782
Sasha Go <a@ptx.su>
2015-04-08 16:39:44 +0600

    less или css

--- src/plusator_app.js
+++ src/plusator_app.js
@@ -4,15 +4,17 @@ requirejs.config({
         knockout: '../bower_components/knockout/dist/knockout',
         'coffee-script': '../bower_components/coffeescript/extras/coffee-script',
         cs: '../bower_components/require-cs/cs',
-        bootstrap: '../bower_components/bootstrap/less/bootstrap'
+        'twbs-less': '../bower_components/bootstrap/less/bootstrap',
+        'twbs-css': '../bower_components/bootstrap/dist/css/bootstrap.min'
     },
     map: {
         '*': {
-            less: '../bower_components/require-less/less'
+            less: '../bower_components/require-less/less',
+            css: '../bower_components/require-css/css.min'
         }
     }
 });
 
-require(['knockout', 'cs!Plusator', 'less!bootstrap'], function (ko, Plusator) {
+require(['knockout', 'cs!Plusator', 1/*0*/ ? 'css!twbs-css' : 'less!twbs-less'], function (ko, Plusator) {
     ko.applyBindings(new Plusator(50, 2));
 });

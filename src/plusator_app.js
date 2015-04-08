ecb817a22d5dde72fa0570b166376bd757e44aab
Sasha Go <a@ptx.su>
2015-04-08 16:16:59 +0600

    Less

--- src/plusator_app.js
+++ src/plusator_app.js
@@ -1,11 +1,18 @@
 requirejs.config({
+    waitSeconds: 15,
     paths: {
         knockout: '../bower_components/knockout/dist/knockout',
         'coffee-script': '../bower_components/coffeescript/extras/coffee-script',
-        cs: '../bower_components/require-cs/cs'
+        cs: '../bower_components/require-cs/cs',
+        bootstrap: '../bower_components/bootstrap/less/bootstrap'
+    },
+    map: {
+        '*': {
+            less: '../bower_components/require-less/less'
+        }
     }
 });
 
-require(['knockout', 'cs!Plusator'], function (ko, Plusator) {
+require(['knockout', 'cs!Plusator', 'less!bootstrap'], function (ko, Plusator) {
     ko.applyBindings(new Plusator(50, 2));
 });

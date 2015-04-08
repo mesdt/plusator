8e817d3517aad45a05e44e0d26ca217fb2da8b90
Sasha Go <a@ptx.su>
2015-04-08 13:18:03 +0600

    С ответом

--- src/Plusator.js
+++ src/Plusator.js
@@ -1,4 +1,10 @@
 function Plusator() {
     this.x = ko.observable(50);
     this.perc = ko.observable(2);
+    this.answer = ko.computed(function () {
+        var x = parseFloat(this.x());
+        var perc = parseFloat(this.perc());
+        var answer = x + x / 100 * perc;
+        return answer;
+    }, this);
 }

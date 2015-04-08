05fec05bc2ffdbac3c644c7cca11ac6ed1f28902
Sasha Go <a@ptx.su>
2015-04-08 14:27:34 +0600

    модулЯ

--- src/Plusator.js
+++ src/Plusator.js
@@ -1,10 +1,11 @@
-function Plusator() {
-    this.x = ko.observable(50);
-    this.perc = ko.observable(2);
-    this.answer = ko.computed(function () {
-        var x = parseFloat(this.x());
-        var perc = parseFloat(this.perc());
-        var answer = x + x / 100 * perc;
-        return answer;
-    }, this);
-}
+define(['knockout'], function (ko) {
+    return function Plusator() {
+        this.x = ko.observable(50);
+        this.perc = ko.observable(2);
+        this.answer = ko.computed(function () {
+            var x = parseFloat(this.x());
+            var perc = parseFloat(this.perc());
+            return x + x / 100 * perc;
+        }, this);
+    };
+});
--- src/plusator_app.js
+++ src/plusator_app.js
@@ -1,1 +1,9 @@
-ko.applyBindings(new Plusator());
+requirejs.config({
+    paths: {
+        knockout: '../bower_components/knockout/dist/knockout'
+    }
+});
+
+require(['knockout', 'Plusator'], function (ko, Plusator) {
+    ko.applyBindings(new Plusator());
+});
